18cb584ea4e56a1bcaf5ad9637f52a0766e7d2b3
dboyliao <qmalliao@gmail.com>
2015-08-18 23:12:03 +0800

    Update slides. Fix Noah's swirl bug for section Function.

--- DSC2015/R_Basic_Part_3-Function/initLesson.R
+++ DSC2015/R_Basic_Part_3-Function/initLesson.R
@@ -2,3 +2,6 @@
 # lesson is started. Any variables created here will show up in
 # the user's working directory and thus be accessible to them
 # throughout the lesson.
+
+hello_world_r <- function(){}
+hello_world_r2 <- function(){ print("Hello World, R") }
